19da103bf442750c1deceb94f8f226ca5d641d3c
Stjefan <44638333+Stjefan@users.noreply.github.com>
2022-11-07 15:27:11 +0100

    yet another branch

--- django_dauerauswertung/services/fun_with_dauerauswertung.py
+++ django_dauerauswertung/services/fun_with_dauerauswertung.py
@@ -1,9 +1,10 @@
 from calendar import monthrange
 import logging
 import sys
+from time import sleep
 from auswertung_service import werte_beurteilungszeitraum_aus
 from insert_auswertung_service import insert_auswertung_via_psycopg2
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 if __name__ == "__main__":
@@ -15,13 +16,13 @@ if __name__ == "__main__":
         ]
     )
     year = 2022
-    month = 10
+    month = 11
     
     fun_2_run_1 = werte_beurteilungszeitraum_aus
     fun_2_run_2 = insert_auswertung_via_psycopg2
-    projektbezeichnung = "mannheim"
-    if True:
-        for d in range(25, min(31, monthrange(year, month)[1])+1):
+    projektbezeichnung = "immendingen"
+    if False:
+        for d in range(7, min(7, monthrange(year, month)[1])+1):
             for h in [0, 1, 2, 3, 4, 5, 21, 22, 23]:
                 try:
                     zeitpunkt = datetime(year, month, d, h, 30, 0)
@@ -38,6 +39,25 @@ if __name__ == "__main__":
             fun_2_run_2(zeitpunkt, ergebnis)
         except Exception as e:
             logging.exception(e)
+    if True:
+        iteration_counter = 0
+        while True:
+            iteration_counter += 1
+            
+            for name in ["immendingen", "mannheim"]:
+                try:
+                    current_moment = datetime.now()
+                    logging.info(f"Iteraiton {iteration_counter} with {current_moment} and {name}")
+                    projektbezeichnung = name
+                    zeitpunkt = current_moment + timedelta(seconds=-300)
+                    ergebnis = fun_2_run_1(zeitpunkt, projektbezeichnung)
+                    fun_2_run_2(zeitpunkt, ergebnis)
+                except Exception as e:
+                    logging.exception(e)
+            logging.info(f"Going to sleep")
+            sleep(300)
+
+
 
 
 
